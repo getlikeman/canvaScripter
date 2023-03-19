f9178420ddb0b73a67668744b9012054757c0802
nikita <misternikir@gmail.com>
2023-03-19 21:24:05 +0600

    1.0

--- Platscripter/src/App.vue
+++ Platscripter/src/App.vue
@@ -1,29 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Filecheker from "./components/Filecheker.vue";
+import ScriptCreator from "./components/ScriptCreator.vue";
+import {ref} from "vue";
+let text=ref()
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col justify-center justify-self-center items-center">
+    <h1 class="text-7xl my-5">
+    TestScriptGenerator
+  </h1>
+  <p class="text-2xl">Данный сайт позволяет создать специальный скрипт с помощью которого проходить тесты не составит труда</p>
+    <p class="text-1xl">перед тем как закинуть файл убедитесь что он правильно оформлен.файл должен быть оформлен следующим образом: </p>
+    <ol class="list-decimal text-2xl">
+      <li>Записывать надо в формате одна строчка вопрос, следующая строчка правильный ответ</li>
+      <li>Надо записывать вопросы и ответы без лишних символов как и в оригинале</li>
+      <li>Скрипт работает следующим образом.При нажатии на вопрос он выбирает нужный вариант  </li>
+    </ol>
+
+    <div class="rounded-lg border-4 border-blue-800 my-10 w-1/2 h-1/2  ">
+    <Filecheker @load="text=$event"/>
+    <ScriptCreator :input="text"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ Platscripter/src/components/Filecheker.vue
@@ -0,0 +1,23 @@
+<script setup>
+let emit=defineEmits(['load'])
+function getText(e){
+    let file = e.target.files[0];
+    console.log(file)
+    let reader=new FileReader()
+  reader.onload = e => emit("load", e.target.result);
+  reader.readAsText(file);
+}
+</script>
+<template>
+<div class="flex flex-col justify-center justify-self-center items-center">
+  <label for="text" class="text-3xl">выберите файл или перетащите его сюда:</label>
+  <input type="file" class="file:bg-green-600 file:shadow-lg file:shadow-green-500-50 file:border-0 hover:file:bg-amber-400 file:rounded-full" name="text"  @change="getText" accept=".txt">
+</div>
+</template>
+
+
+
+<style scoped>
+
+</style>
+
