3f50e5310d3b6805fc6acedde4da09dc8285288f
getlikeman <misternikir@gmail.com>
2023-05-22 17:09:55 +0600

    2.1

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ let text = ref()
 <template>
     <div class="flex flex-col justify-center justify-self-center items-center">
         <h1 class="text-7xl my-5">
-            TestScriptGenerator
+            Test Script Generator
         </h1>
         <p class="text-2xl">Данный сайт позволяет создать специальный скрипт с помощью которого проходить тесты не
             составит труда</p>
@@ -19,7 +19,6 @@ let text = ref()
             <li>Записывать надо в формате одна строчка вопрос, следующая строчка правильный ответ</li>
             <li>Надо записывать вопросы и ответы без лишних символов как и в оригинале</li>
             <li>Скрипт работает следующим образом.При нажатии на вопрос он выбирает нужный вариант</li>
-            <li>Из файла нужно убрать нужно убрать все пробелы</li>
         </ol>
 
         <div class="rounded-lg border-4 border-blue-800 my-10 w-1/2 h-1/2  ">
